0445e30d75744e8c1eb0b59f764464cdd128c90e
Torge Schöwing <ts@netzmal.de>
2026-01-22 23:48:27 +0100

    ex 2.8, ex 2.9

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -3,9 +3,14 @@ import Person from './components/Person.jsx'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas'}
+        {name: 'Arto Hellas', number: '040-123456', id: 1},
+        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
+        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [newFilter, setNewFilter] = useState('')
 
     const addPerson = (event) => {
         event.preventDefault()
@@ -15,6 +20,7 @@ const App = () => {
         } else {
             const personObject = {
                 name: newName,
+                number: newNumber,
                 id: String(persons.length + 1),
             }
 
@@ -29,14 +35,41 @@ const App = () => {
         setNewName(event.target.value)
     }
 
+    const handleNumberChange = (event) => {
+        console.log(event.target.value)
+        setNewNumber(event.target.value)
+    }
+
+    const handleFilterChange = (event) => {
+        console.log(event.target.value)
+        setNewFilter(event.target.value)
+    }
+
+    const filteredPersons = persons.filter(person => person.name.includes(newFilter))
+
     return (
         <div>
             <h2>Phonebook</h2>
+
+            <div>
+                filter shown with: <input
+                value={newFilter}
+                onChange={handleFilterChange}
+            />
+            </div>
+
+            <h2>add a new</h2>
             <form onSubmit={addPerson}>
                 <div>
                     name: <input
                     value={newName}
                     onChange={handleNameChange}
+                />
+                </div>
+                <div>
+                    number: <input
+                    value={newNumber}
+                    onChange={handleNumberChange}
                 />
                 </div>
                 <div>
@@ -46,7 +79,7 @@ const App = () => {
 
             <h2>Numbers</h2>
             <div>
-                {persons.map(person => <Person key={person.name} person={person}/>)}
+                {filteredPersons.map(person => <Person key={person.name} person={person}/>)}
             </div>
         </div>
     )
